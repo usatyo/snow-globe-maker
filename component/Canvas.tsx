88ae2486def6e3e22b3aafc1554f658505b81a39
usatyo <yu0424.math8128@gmail.com>
2022-11-08 01:10:13 +0900

    add sample image

--- component/Canvas.tsx
+++ component/Canvas.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image"
 import { FC } from "react"
 import Crystal from "./Crystal"
+import sampleImg from "../assets/sample.png"
 
 type Props = {
   className?: string
@@ -8,8 +10,8 @@ type Props = {
 export const Canvas: FC<Props> = ({ className }) => {
   return (
     <div className={`relative h-full w-full flex flex-row ${className}`}>
-      <div className="absolute top-10 bottom-10 left-10 right-11 z-30 bg-black items-stretch">
-        <p className="m-10 text-white text-3xl">three.js</p>
+      <div className="absolute top-10 bottom-10 left-10 right-11 z-30 bg-black items-stretch border-2 border-accent-original">
+        <Image src={sampleImg} alt="sample" layout="fill" objectFit="cover" />
       </div>
       <Crystal />
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Crystal from "../component/Crystal";
 import Header from "../component/Header";
 
