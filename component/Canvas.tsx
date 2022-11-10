6429d0117d5dc43bb2c64427e731757cafa93504
usatyo <yu0424.math8128@gmail.com>
2022-11-10 14:25:04 +0900

    add background image

--- component/Canvas.tsx
+++ component/Canvas.tsx
@@ -1,15 +1,17 @@
 import { FC, useEffect, useRef } from "react"
 import Crystal from "./Crystal"
-import { AmbientLight, DirectionalLight, PerspectiveCamera, PointLight, Scene, WebGLRenderer } from "three"
+import { AmbientLight, CubeTextureLoader, PerspectiveCamera, PointLight, Scene, WebGLRenderer } from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 type Props = {
   path: string
+  onLoading?: (percent: number) => void
+  onError?: (error: ErrorEvent) => void
   className?: string
 }
 
-export const Canvas: FC<Props> = ({ path, className }) => {
+export const Canvas: FC<Props> = ({ path, onLoading, onError, className }) => {
   const containerRef = useRef<HTMLDivElement>(null)
   useEffect(() => {
     if (!containerRef.current) throw Error("no ref")
@@ -22,26 +24,46 @@ export const Canvas: FC<Props> = ({ path, className }) => {
     const camera = new PerspectiveCamera(45, containerRef.current.clientWidth / containerRef.current.clientHeight, 1, 1000)
     camera.position.set(0, 0, 10)
 
-    const loader = new GLTFLoader()
-
-    loader.load(
+    // gltfファイルの読み込み、シーンへの追加
+    const gltfLoader = new GLTFLoader()
+    gltfLoader.load(
       path,
       (tmpGltf) => {
         scene.add(tmpGltf.scene)
-        console.log("loaded")
-      }, () => { },
+        console.log("gltf loaded")
+      },
+      (xhr) => {
+        // オブジェクトを読み込んでいるタイミングで実行
+        onLoading?.(xhr.loaded / xhr.total * 100)
+        console.log(`${(xhr.loaded / xhr.total * 100)}% loaded`)
+      },
       (error) => {
+        // 読み込みエラー時
+        onError?.(error)
         console.log(error)
       }
     )
 
-    // 平行光源
-    // const directionalLight = new DirectionalLight(0xFFFFFF, 2)
-    // directionalLight.position.set(1, 1, 1)
-    // // シーンに追加
-    // scene.add(directionalLight)
+    // 背景画像の読み込み、設定
+    const skyLoader = new CubeTextureLoader()
+    skyLoader.load(
+      [
+        'px.png',
+        'nx.png',
+        'py.png',
+        'ny.png',
+        'pz.png',
+        'nz.png'
+      ],
+      (tmpTexture) => {
+        scene.background = tmpTexture
+        console.log("texture loaded")
+      }, () => { },
+      (error) => { }
+    )
 
 
+    // ライトを作成
     const pointLight1 = new PointLight(0xFFFFFF, 5, 10, 0.1);
     pointLight1.position.set(2, 1, 2)
     scene.add(pointLight1)
@@ -57,6 +79,7 @@ export const Canvas: FC<Props> = ({ path, className }) => {
     const ambientLight = new AmbientLight(0xFFFFFF, 0.5);
     scene.add(ambientLight)
 
+    // コントロール（回転のみ許可）
     const controls = new OrbitControls(camera, renderer.domElement)
     controls.target.set(0, 0, 0)
     controls.enablePan = false
