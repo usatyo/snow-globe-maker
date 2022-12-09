be91d276c880d12204514eb6b67611200a0c2ed6
karintou8710 <karintou8710@gmail.com>
2022-12-09 23:58:22 +0900

    [fix] snow movement

--- component/canvas/Snows.tsx
+++ component/canvas/Snows.tsx
@@ -4,6 +4,21 @@ import { useFrame } from '@react-three/fiber'
 type Vector3 = [number, number, number]
 type SnowProps = {
   position: Vector3
+  size: number
+}
+
+// 定数
+const SNOW_POSITION_SCALE = 21.5
+const SNOW_OFFSET_FROM_ORIGIN: Vector3 = [0, 3, 0]
+const SNOW_NUMBER = 1000
+const SNOW_MIN_POSITION_Y = -2
+const SNOW_SPEED = 0.005
+
+const inSnowGlobe = (position: Vector3, r: number, offset: Vector3) => {
+  const x = position[0] - offset[0]
+  const y = position[1] - offset[1]
+  const z = position[2] - offset[2]
+  return x * x + y * y + z * z <= r * r
 }
 
 const generateRandomSpherePosition = (): Vector3 => {
@@ -14,35 +29,39 @@ const generateRandomSpherePosition = (): Vector3 => {
     x = Math.random() - 0.5
     y = Math.random() - 0.5
     z = Math.random() - 0.5
-    if (x * x + y * y + z * z <= 0.5 * 0.5) break
+    if (inSnowGlobe([x, y, z], 0.5, [0, 0, 0])) break
   }
 
   return [x, y, z]
 }
 
-const generateSnowPosition = (): Vector3 => {
-  const scale = 18
-  const offsetY = 5
-  let [x, y, z] = generateRandomSpherePosition()
-  x = x * scale
-  y = y * scale + offsetY
-  z = z * scale
+const generateRandomTopSpherePosition = (): Vector3 => {
+  const x = (Math.random() - 0.5) * 0.8
+  const z = (Math.random() - 0.5) * 0.8
+  const y = Math.sqrt(0.5 * 0.5 - x * x - z * z)
+  return [x, y, z]
+}
+
+const generateSnowPosition = (isInitial?: boolean): Vector3 => {
+  let [x, y, z] = isInitial ? generateRandomSpherePosition() : generateRandomTopSpherePosition()
+  x = x * SNOW_POSITION_SCALE + SNOW_OFFSET_FROM_ORIGIN[0]
+  y = y * SNOW_POSITION_SCALE + SNOW_OFFSET_FROM_ORIGIN[1]
+  z = z * SNOW_POSITION_SCALE + SNOW_OFFSET_FROM_ORIGIN[2]
 
   return [x, y, z]
 }
 
-const Snow = forwardRef<THREE.Mesh, SnowProps>(({ position }, ref) => {
+const Snow = forwardRef<THREE.Mesh, SnowProps>(({ position, size }, ref) => {
   return (
     <mesh position={position} ref={ref}>
-      <sphereBufferGeometry args={[0.06, 24, 24]} />
+      <sphereBufferGeometry args={[size, 24, 24]} />
       <meshStandardMaterial color={'white'} />
     </mesh>
   )
 })
 
 const Snows = () => {
-  const numberOfSnow = 300
-  const snowsBase = [...new Array(numberOfSnow)]
+  const snowsBase = [...new Array(SNOW_NUMBER)]
   const snowRefs = useRef<RefObject<THREE.Mesh>[]>([])
 
   snowsBase.forEach((_, i) => {
@@ -53,14 +72,17 @@ const Snows = () => {
   useFrame(() => {
     snowRefs.current.forEach((ref) => {
       if (!ref.current) return
-      const minY = -3
-      const maxY = 12
-      const stepY = 0.01
-      const y = ref.current.position.y
-      if (y < minY) {
-        ref.current.position.y = maxY
+      const position: Vector3 = [ref.current.position.x, ref.current.position.y, ref.current.position.z]
+      if (
+        !inSnowGlobe(position, 0.5 * SNOW_POSITION_SCALE, SNOW_OFFSET_FROM_ORIGIN) ||
+        SNOW_MIN_POSITION_Y > position[1]
+      ) {
+        const np = generateSnowPosition(false)
+        ref.current.position.x = np[0]
+        ref.current.position.y = np[1]
+        ref.current.position.z = np[2]
       } else {
-        ref.current.position.y -= stepY
+        ref.current.position.y -= SNOW_SPEED
       }
     })
   })
@@ -68,7 +90,9 @@ const Snows = () => {
   return (
     <>
       {snowsBase.map((_, i) => {
-        return <Snow position={generateSnowPosition()} key={i} ref={snowRefs.current[i]} />
+        return (
+          <Snow size={Math.random() * 0.1} position={generateSnowPosition(true)} key={i} ref={snowRefs.current[i]} />
+        )
       })}
     </>
   )
