ebc3c109ee75860be2c80b5cb6e8e92239157b28
usatyo <yu0424.math8128@gmail.com>
2023-02-10 15:45:03 +0900

    merge header

--- component/Header.tsx
+++ component/Header.tsx
@@ -18,20 +18,20 @@ export const Header: FC<Props> = ({ className }) => {
       </Link>
       <div className="flex flex-row space-x-4 md:space-x-16 text-xl tracking-wider">
         <Link href="/" className="flex flex-col items-center mt-2">
-          <p className="text-accent-original text-[4px] md:text-[8px] -mt-1  ml-2 ">タイトル</p>
-          <p className="relative font-italianno text-md md:text-3xl hover:after:opacity-100 after:absolute after:h-[1px] md:after:w-[120%] after:top-11 after:left-[-10%] after:bg-accent-original after:opacity-0 after:transition-all after:duration-700">
+          <p className="text-accent-original text-[0px] md:text-[8px] -mt-1  ml-2 invisible md:visible">タイトル</p>
+          <p className="relative font-italianno text-2xl md:text-3xl hover:after:opacity-100 after:absolute after:h-[1px] md:after:w-[120%] after:top-11 after:left-[-10%] after:bg-accent-original after:opacity-0 after:transition-all after:duration-700">
             Title
           </p>
         </Link>
         <Link href="/gallery" className="flex flex-col items-center mt-2 ">
-          <p className="text-accent-original text-[4px] md:text-[8px] -mt-1  ml-2">ギャラリー</p>
-          <p className="relative font-italianno text-md md:text-3xl hover:after:opacity-100 after:absolute after:h-[1px] md:after:w-[120%] after:top-11 after:left-[-10%] after:bg-accent-original after:opacity-0 after:transition-all after:duration-700">
+          <p className="text-accent-original text-[0px] md:text-[8px] -mt-1  ml-2 invisible md:visible">ギャラリー</p>
+          <p className="relative font-italianno text-2xl md:text-3xl hover:after:opacity-100 after:absolute after:h-[1px] md:after:w-[120%] after:top-11 after:left-[-10%] after:bg-accent-original after:opacity-0 after:transition-all after:duration-700">
             Gallery
           </p>
         </Link>
         <Link href="/original" className="flex flex-col items-center mt-2 ">
-          <p className="text-accent-original text-[4px] md:text-[8px] -mt-1  ml-2">オリジナル</p>
-          <p className="relative font-italianno text-md md:text-3xl hover:after:opacity-100 after:absolute after:h-[1px] md:after:w-[120%] after:top-11 after:left-[-10%] after:bg-accent-original after:opacity-0 after:transition-all after:duration-700">
+          <p className="text-accent-original text-[0px] md:text-[8px] -mt-1  ml-2 invisible md:visible">オリジナル</p>
+          <p className="relative font-italianno text-2xl md:text-3xl hover:after:opacity-100 after:absolute after:h-[1px] md:after:w-[120%] after:top-11 after:left-[-10%] after:bg-accent-original after:opacity-0 after:transition-all after:duration-700">
             Original
           </p>
         </Link>
