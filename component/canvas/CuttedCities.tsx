b357ac0d02557e3c660f3632a5310790ef417571
karintou8710 <karintou8710@gmail.com>
2023-02-15 05:37:10 +0900

    [fix] small fix

--- component/canvas/CuttedCities.tsx
+++ component/canvas/CuttedCities.tsx
@@ -8,10 +8,10 @@ const CuttedCities = (props: { paths: string[] }) => {
     <>
       <group>
         <Bounds fit clip observe damping={6} margin={1.7} position={[0, 0, 0]}>
-          <group position={[0.2, -1, 0]}>
+          <group position={[0, 0, 0]}>
             {props.paths.map((path, idx) => {
               return (
-                <Model rotation-y={Math.PI} position={[-0.3, 0, 0]} scale={0.1} object={null} path={path} key={idx} />
+                <Model rotation-y={Math.PI} position={[-0, -3, 0]} scale={0.05} object={null} path={path} key={idx} />
               )
             })}
           </group>
