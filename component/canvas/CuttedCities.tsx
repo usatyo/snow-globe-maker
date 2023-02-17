c2de342daf6020cab6cc3c8195fbc35c9d5beaee
karintou8710 <karintou8710@gmail.com>
2023-02-17 16:50:42 +0900

    [fix] cutted cities scale

--- component/canvas/CuttedCities.tsx
+++ component/canvas/CuttedCities.tsx
@@ -4,13 +4,14 @@ import { scaleToRadius, LocationCoordinates } from '../../util/util'
 
 import Model from './Model'
 
-const CuttedCities = (props: { paths: string[], pos: PositionType}) => {
+const CuttedCities = (props: { paths: string[]; pos: PositionType }) => {
   const originLocCor = new LocationCoordinates(origin.lat, origin.lng, props.pos.alt)
   const posLocCor = new LocationCoordinates(props.pos.lat, props.pos.lng, props.pos.alt).toVec3Array(originLocCor)
   // 「現実の半径」を「スノードームのモデルの地面の半径」へ縮小する割合
   // 空のスノードームのモデルの地面の半径が目測1.886m,
   // スノードームを表示するときにscale=5としているため5倍
-  const scaleMultiplyer = (1.886 * 5) / scaleToRadius(props.pos.scale)
+  // 5倍だと一部の建物がはみ出るので、4.4に手動調整
+  const scaleMultiplyer = (1.886 * 4.4) / scaleToRadius(props.pos.scale)
 
   return (
     <>
@@ -19,10 +20,14 @@ const CuttedCities = (props: { paths: string[], pos: PositionType}) => {
           <group position={[0, 0, 0]}>
             {props.paths.map((path, idx) => {
               return (
-                <Model 
+                <Model
                   rotation-y={Math.PI}
                   //台のy位置は約-0.3249m
-                  position={[-posLocCor[0] * scaleMultiplyer, -(origin.alt)*scaleMultiplyer-1-0.3249*5, -posLocCor[2] * scaleMultiplyer]}
+                  position={[
+                    -posLocCor[0] * scaleMultiplyer,
+                    -origin.alt * scaleMultiplyer - 1 - 0.3249 * 5,
+                    -posLocCor[2] * scaleMultiplyer
+                  ]}
                   scale={scaleMultiplyer}
                   object={null}
                   path={path}
